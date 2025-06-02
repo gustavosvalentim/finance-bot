4d71c99ec66e9c258dedc4657c3d7690e038ce96
Gustavo Valentim <gustavosvalentim1@gmail.com>
2025-06-02 10:28:24 -0300

    fix: gunicorn daemon

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ python manage.py migrate
 python manage.py collectstatic --noinput
 
 # Run Django server
-gunicorn finance_bot.wsgi:application --bind 0.0.0.0:8000 --workers 1 -d
+gunicorn finance_bot.wsgi:application --bind 0.0.0.0:8000 --workers 1 --daemon
 
 # Run Telegram Bot
 python manage.py telegram_bot
