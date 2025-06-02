729528cf410e3c440e51d455865bb2ecd03a7f52
Gustavo Valentim <gustavosvalentim1@gmail.com>
2025-06-02 10:27:59 -0300

    fix: change gunicorn to daemon mode

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ python manage.py migrate
 
 python manage.py collectstatic --noinput
 
+# Run Django server
+gunicorn finance_bot.wsgi:application --bind 0.0.0.0:8000 --workers 1 -d
+
 # Run Telegram Bot
 python manage.py telegram_bot
-
-# Run Django server
-gunicorn finance_bot.wsgi:application --bind 0.0.0.0:8000 --workers 1
