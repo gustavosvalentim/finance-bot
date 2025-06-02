af3fabbf9d8b927f0e67e8130da4e3a442375376
Gustavo Valentim <gustavosvalentim1@gmail.com>
2025-06-02 10:39:14 -0300

    feat: add user and user interaction to admin

--- finance_bot/telegram_bot/admin.py
+++ finance_bot/telegram_bot/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 
 from finance_bot.telegram_bot.models import TelegramUserSettings
-from finance_bot.users.models import UserInteraction
 
 
 admin.site.register(TelegramUserSettings)
-admin.site.register(UserInteraction)
--- finance_bot/users/admin.py
+++ finance_bot/users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from finance_bot.users.models import User, UserInteraction
+
+
+admin.site.register(User)
+admin.site.register(UserInteraction)
