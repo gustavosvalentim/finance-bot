e8bbc381fff131bb81f8cce12209d984418ef52a
Gustavo Valentim <gustavosvalentim1@gmail.com>
2025-06-13 19:09:51 -0300

    fix: daphne command

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 # Run Django server
-daphne finance_bot.asgi:application --bind 0.0.0.0:8000 --workers 1 --daemon
+nohup daphne finance_bot.asgi:application --bind 0.0.0.0:8000 &
 
 # Run Telegram Bot
 python manage.py telegram_bot
