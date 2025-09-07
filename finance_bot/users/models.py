901c6606e690c473774c9d8d407b7280b6e68de1
Gustavo Valentim <gustavosvalentim1@gmail.com>
2025-09-07 16:38:17 -0300

    fix: remove username and set email as username field

--- finance_bot/users/models.py
+++ finance_bot/users/models.py
@@ -7,9 +7,14 @@ class User(AbstractUser):
     User model to store user information.
     """
 
+    username = None
+
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=15, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return f'{self.first_name} {self.last_name} ({self.email})'
